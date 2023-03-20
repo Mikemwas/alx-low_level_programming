3b76fe2b58d853787aed057918f8c944193bfe8b
Mikemwas <michaelmwangi628@gmail.com>
2023-03-20 05:54:38 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,7 +5,7 @@
 /**
  * main - A program that prints the alphabet in lowercase, and then
  * in uppercase, followed by a new line.
- * Return - Always 0 (success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
