82674f55bc525d419e49635aaf8b42a66cfdc4fa
Mikemwas <michaelmwangi628@gmail.com>
2023-03-20 07:27:31 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,12 @@
 
 int main(void)
 
-{
-        char low, e, q;
+{	
+	char low, e, q;
 
 	e = 'e';
 	q = 'q';
-
-        for (low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
         if (low != e && low != q)
         putchar(low);
