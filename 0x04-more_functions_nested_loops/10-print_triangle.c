9e1b12d5177c830f754978ef88e4fd526e8583d2
Mikemwas <michaelmwangi628@mail.com>
2023-03-25 23:17:47 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints n squares according n number of times
+ * @size: The number of squares/number of times
+ * Return: empty
+ */
+
+void print_triangle(int size)
+
+{
+	int x, y z;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = size - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; x++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
