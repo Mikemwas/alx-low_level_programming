14053151a582c2b3ff412144056189be39ffe033
Mikemwas <michaelmwangi628@mail.com>
2023-04-03 13:20:47 -0700

    diagnol

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,14 +7,21 @@
  * @size: input
  * Return: Always 0 (success)
  */
-void print_diagsums(int *a, int size)
-{
-	int i, n, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size * size); i = i + 1)
-		sum1 = sum1 + a[i];
-
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-	sum2 = sum2 + a[n];
-	printf("%d, %d\n", sum1, sum2);
+void print_diagsums(int *a, int size) {
+    int i, j;
+    int sum1 = 0, sum2 = 0;
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            if (i == j) {
+                sum1 += *(a + i*size + j);  // sum elements on the main diagonal
+            }
+            if (i == size - j - 1) {
+                sum2 += *(a + i*size + j);  // sum elements on the secondary diagonal
+            }
+        }
+    }
+    printf("Sum of main diagonal: %d\n", sum1);
+    printf("Sum of secondary diagonal: %d\n", sum2);
 }
+
