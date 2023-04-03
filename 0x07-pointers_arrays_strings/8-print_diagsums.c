94ef824b94421366404a2f76624f6d895b6a33ea
Mikemwas <michaelmwangi628@mail.com>
2023-04-03 13:56:49 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,26 +2,19 @@
 #include "main.h"
 
 /**
- * print_diagsums - entry point
+ * print_diagsums - Entry point
  * @a: input
  * @size: input
  * Return: Always 0 (success)
  */
+void print_diagsums(int *a, int size)
+{
+	int i, n, sum1 = 0, sum2 = 0;
 
-void print_diagsums(int *a, int size) {
-    int i, j;
-    int sum1 = 0, sum2 = 0;
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++) {
-            if (i == j) {
-                sum1 += *(a + i*size + j);  // sum elements on the main diagonal
-            }
-            if (i == size - j - 1) {
-                sum2 += *(a + i*size + j);  // sum elements on the secondary diagonal
-            }
-        }
-    }
-    printf("Sum of main diagonal: %d\n", sum1);
-    printf("Sum of secondary diagonal: %d\n", sum2);
+	for (i = 0; i <= (size * size); i = i + 1)
+		sum1 = sum1 + a[i];
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	sum2 = sum2 + a[n];
+	printf("%d, %d\n", sum1, sum2);
 }
-
