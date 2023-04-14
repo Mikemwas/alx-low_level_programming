c25eff3bb91eb0f417c894b42c1e35f146e04e4a
Mikemwas <michaelmwangi628@mail.com>
2023-04-14 07:44:23 -0700

    malloc cheked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,17 +7,13 @@
  *
  * Return: a pointer to the allocated memory
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-	ptr = malloc(b);
+	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		exit(98);
-	}
+
 	return (ptr);
 }
 
