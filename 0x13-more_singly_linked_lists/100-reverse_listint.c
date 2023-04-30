497642154f5c15b439ddae241a848841074bf827
mikemwas <michaelmwangi628@gmail.com>
2023-04-29 22:22:42 -0700

    add all

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - yes
+ * @head: kichwa
+ *
+ * Return: give out the result immediately
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
+}
+
